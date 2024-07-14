adb9e2f92a928633e24948a53350bf6ee9698413
Not white <whit@privilege.com>
2024-07-14 19:26:42 +0200

    feat: added handling and types for commands
    Added CLI argument for command modes:
    - emesse: Sent
    - ricevute: Received
    
    Code refactor and extenions

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.Globalization;
 using CsvHelper;
 using CsvHelper.Configuration;
+using Org.BouncyCastle.Tls;
 
 namespace FatturazioneXML;
 
@@ -8,23 +9,47 @@ class Program
 {
     public static CsvConfiguration CSVConfig = new(CultureInfo.GetCultureInfo("it-IT")) { Delimiter = ";" };
 
-    public static void Usage() {
-        Console.WriteLine("usage: <xml_dir> <csv_output>");
+    public static void Usage()
+    {
+        Console.WriteLine("usage: <xml_dir> <csv_output> <mode>");
+        Console.WriteLine("availables <mode>: {emesse, ricevute}");
+        Console.WriteLine("example: ../emesse ./emesse.csv emesse");
+    }
+
+
+    delegate void RunCommand(string XML_DIRECTORY, string CSV_OUTPUT_PATH);
+
+    public static void RunRicevute(string XML_DIRECTORY, string CSV_OUTPUT_PATH)
+    {
+        List<FatturaRidotta> fatture = Handler.LoadFattureRidotte(XML_DIRECTORY);
+        string CSVContent = FattureRidottaToCSV(fatture);
+        WriteToFile(CSV_OUTPUT_PATH, CSVContent);
+    }
+
+
+    public static void RunEmesse(string XML_DIRECTORY, string CSV_OUTPUT_PATH)
+    {
+        List<FatturaRidottaWithDescription> fatture = Handler.LoadFattureRidotteWithDescription(XML_DIRECTORY);
+        string CSVContent = FattureRidottaWithDescriptionToCSV(fatture);
+        WriteToFile(CSV_OUTPUT_PATH, CSVContent);
     }
 
     static void Main(string[] args)
     {
-
-        if (args.Length < 2) {
+        if (args.Length < 3 || args[0].Equals("--help"))
+        {
             Usage();
             return;
         }
 
         string XML_DIRECTORY = args[0];
         string CSV_OUTPUT_PATH = args[1];
-        List<FatturaRidotta> fatture = Handler.LoadFattureRidotte(XML_DIRECTORY);
-        string CSVContent = FattureRidottaToCSV(fatture);
-        WriteToFile(CSV_OUTPUT_PATH, CSVContent);
+        string MODE = args[2];
+
+        // TODO: Error Handling CLI Input
+
+        RunCommand run = MODE.Equals("emesse") ? RunEmesse : RunRicevute;
+        run(XML_DIRECTORY, CSV_OUTPUT_PATH);
     }
 
 
@@ -41,6 +66,16 @@ class Program
     {
         using var writer = new StringWriter();
         using var csv = new CsvWriter(writer, CSVConfig);
+        csv.WriteRecords(fatture);
+        return writer.ToString();
+    }
+
+
+    public static string FattureRidottaWithDescriptionToCSV(IEnumerable<FatturaRidottaWithDescription> fatture)
+    {
+        using var writer = new StringWriter();
+        using var csv = new CsvWriter(writer, CSVConfig);
+        csv.Context.RegisterClassMap<Mapper.FatturaRidottaWithDescriptionMapper>();
         csv.WriteRecords(fatture);
         return writer.ToString();
     }
